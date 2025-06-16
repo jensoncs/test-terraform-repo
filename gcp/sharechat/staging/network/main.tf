124086eb4826f334844d558fc9c63e024dc1f8ee
Test User <test@example.com>
2025-06-16 17:02:51 +0530

    [Jenson] Add more example

--- gcp/sharechat/staging/network/main.tf
+++ gcp/sharechat/staging/network/main.tf
@@ -6,3 +6,12 @@ resource "random_string" "random_string" {
 output "name" {
   value = random_string.random_string.result
 }
+
+resource "random_string" "random_string1" {
+  length  = 20
+  special = false
+}
+
+output "name1" {
+  value = random_string.random_string1.result
+}
